663ded09ad2364f3f4d399d06ef116ac2d342d8a
LEE-FE17 <mi6354828@gmail.com>
2026-03-09 14:54:11 +0900

    feat: TMDB API에서 인기 영화 데이터를 가져오고 성인 콘텐츠 필터링

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
-import { useState } from "react";
-import movieListData from "./data/movieListData.json";
+import { useState, useEffect } from "react";
 import MovieCard from "./components/MovieCard";
 import "./App.css";
 
 function App() {
-  const [movies] = useState(movieListData.results);
+  const [movies, setMovies] = useState([]);
+
+  const accessToken = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
+        {
+          headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      const data = await response.json();
+
+      // adult false 필터
+      const filteredMovies = data.results.filter((movie) => !movie.adult);
+
+      setMovies(filteredMovies);
+    };
+
+    fetchMovies();
+  }, [accessToken]);
 
   return (
     <div>
-      <h1>인기순</h1>
+      <h1>인기 영화</h1>
 
       <div className="movie-list">
         {movies.map((movie) => (
           <MovieCard
             key={movie.id}
+            id={movie.id}
             title={movie.title}
-            poster={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            poster={movie.poster_path}
             rating={movie.vote_average}
           />
         ))}
