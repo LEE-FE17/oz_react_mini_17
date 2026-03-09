d1aaaf3cbcf944d596c2ef591fbe2ed22a35ff3c
LEE-FE17 <mi6354828@gmail.com>
2026-03-09 15:02:47 +0900

    feat: TMDB 영화 상세 API를 사용한 영화 상세 페이지 구현

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,13 +1,40 @@
-import { useState } from "react";
-import movieDetailData from "../data/movieDetailData.json";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function MovieDetail() {
-  const [movie] = useState(movieDetailData);
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  const accessToken = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
+
+  useEffect(() => {
+    const fetchMovieDetail = async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}?language=ko-KR`,
+        {
+          headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      const data = await response.json();
+      setMovie(data);
+    };
+
+    fetchMovieDetail();
+  }, [id, accessToken]);
+
+  if (!movie) return <div>Loading...</div>;
 
   return (
     <div>
-      <img src={movie.poster_path} alt={movie.title} />
       <h1>{movie.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+      />
       <p>{movie.overview}</p>
       <p>⭐ {movie.vote_average}</p>
     </div>
